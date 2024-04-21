5e597918fc478a1fe72cefda23a43505d2f33959
Will Alicar <will@alicar.me>
2024-04-21 12:36:34 -0700

    make Component class header only

--- src/component.h
+++ src/component.h
@@ -29,11 +29,41 @@ public:
 private:
     static std::vector<Entry> data;
 public:
-    static T& get(size_t id);
-    static void insert(T comp);
-    static void remove(size_t id);
-    static void fixed_update();
-    static void delta_update(float delta);
+    static T& get(size_t id) {
+        for (Entry entry : data) {
+            if ( id == entry.comp.get_id() && entry.valid) {
+                return entry;
+            }
+        }
+        return nullptr;
+    };
+
+    static void insert(T comp) {
+        data.push_back(comp);
+    };
+
+    static void remove(size_t id) {
+        size_t it = std::find_if(data.begin(), data.end(), [id](const Entry& entry) {return entry.get_id() == id;});
+        if (it != data.end()) {
+            data.erase(it);
+        }
+    }
+
+    static void fixed_update() {
+        for (Entry& entry : data) {
+            if (entry.valid) {
+                entry.comp.fixed_update();
+            }
+        }
+    }
+
+    static void delta_update(float delta) {
+        for (Entry& entry : data) {
+            if (entry.valid) {
+                entry.comp.delta_update(delta);
+            }
+        }
+    }
 };
 
 // Used to retrieve component sets
