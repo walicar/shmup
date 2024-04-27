0b8ebbfdff900d798521cff143433df4aa175500
Will Alicar <will@alicar.me>
2024-04-26 17:39:09 -0700

    compile-time hash of literal strings

--- src/ecs/types.h
+++ src/ecs/types.h
@@ -29,5 +29,31 @@ using EventId = std::uint32_t;
 using ParamId = std::uint32_t;
 
 
+// C++11 compile-time hash of literal strings
+constexpr std::uint32_t fnv1a_32(char const* s, std::size_t count)
+{
+    return ((count ? fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u; // NOLINT (hicpp-signed-bitwise)
+}
+
+constexpr std::uint32_t operator "" _hash(char const* s, std::size_t count)
+{
+    return fnv1a_32(s, count);
+}
+
+namespace Events::Window {
+    const EventId QUIT = "Events::Window::QUIT"_hash;
+    const EventId RESIZED = "Events::Window::RESIZED"_hash;
+    const EventId INPUT = "Events::Window::INPUT"_hash;
+}
+
+namespace Events::Window::Input {
+    const ParamId INPUT = "Events::Window::Input::INPUT"_hash;
+}
+
+namespace Events::Window::Resized {
+    const ParamId WIDTH = "Events::Window::Resized::WIDTH"_hash;
+    const ParamId HEIGHT = "Events::Window::Resized::HEIGHT"_hash;
+}
+
 
 #endif
