df58df5807e1b83bb6f1dd3457f791aa2566fbdd
Will Alicar <will@alicar.me>
2024-04-27 17:11:56 -0700

    update components

--- src/components/hitbox.h
+++ src/components/hitbox.h
@@ -1,11 +1,11 @@
-#ifndef TRANSFORM_H
-#define TRANSFORM_H
+#ifndef HITBOX_H
+#define HITBOX_H
 
 #include <gtc/matrix_transform.hpp>
 #include <gtc/type_ptr.hpp>
 
 struct Transform {
-    glm::vec3 pos;
+    glm::vec3 hitbox;
 };
 
 #endif
--- src/components/player.h
+++ src/components/player.h
@@ -1,1 +1,6 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
 struct Player {}; // component handle so we can fetch player
+
+#endif
