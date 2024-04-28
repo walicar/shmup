198fa93144a7a16db457856aef13f7063eb36893
Will Alicar <will@alicar.me>
2024-04-28 07:40:03 -0700

    render laser attack

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,16 @@
 #include "ecs/coordinator.h"
 #include "ecs/types.h"
 #include "components/sprite.h"
-#include "components/player.h"
+#include "components/velocity.h"
+#include "components/hitbox.h"
+#include "components/tags/player.h"
+#include "components/tags/enemy.h"
+#include "components/tags/projectile.h"
+#include "components/tags/controllable.h"
 #include "components/transform.h"
 #include "systems/sprite_system.h"
-#include "systems/control_system.h"
+#include "systems/movement_system.h"
+#include "systems/projectile_system.h"
 #include <chrono>
 
 Coordinator GCR;
@@ -27,17 +33,36 @@ int main() {
 
     Shader def_shader = ResourceManager::load_shader("shaders/default.vert", "shaders/default.frag", "default");
     Texture def_texture = ResourceManager::load_texture("textures/smile.png", false, "smile");
+    Texture laser_texture = ResourceManager::load_texture("textures/laser.png", false, "laser");
 
     float v[] = {
-            -0.5f, -0.5f, 0.0f,     0.5, 0.0,       // bottom left
-            0.5f, -0.5f, 0.0f,      1.0, 0.5,       // bottom right
-            0.0f, 0.5f, 0.0f,       0.5, 1.0        // upper middle
+            -0.25f, -0.5f, 0.0f, 0.5, 0.0,       // bottom left
+            0.25f, -0.5f, 0.0f, 1.0, 0.5,       // bottom right
+            0.0f, 0.5f, 0.0f, 0.5, 1.0        // upper middle
     };
 
-    GCR.register_component<Sprite>();
-    GCR.register_component<Player>();
-    GCR.register_component<Transform>();
+    float lv[] = {
+            -0.25f, -1.0f, 0.0f, 0.0f, 0.0f,
+            0.25f, -1.0f, 0.0f, 1.0f, 0.0f,
+            0.25f, 1.0f, 0.0f, 1.0f, 1.0f,
+            -0.25f, 1.0f, 0.0f, 0.0f, 1.0f
+    };
 
+    // register components
+    GCR.register_component<Sprite>();
+    GCR.register_component<Transform>();
+    GCR.register_component<Hitbox>();
+    GCR.register_component<Velocity>();
+    GCR.register_component<Player>();
+    GCR.register_component<Enemy>();
+    GCR.register_component<Projectile>();
+    GCR.register_component<Controllable>();
+
+    // register systems
+    /**
+     * NOTE: any entity that has all components required by a system
+     *  will be added to the that system's entity list
+     */
     auto sprite_system = GCR.register_system<SpriteSystem>();
     {
         Signature signature;
@@ -46,39 +71,86 @@ int main() {
         GCR.set_system_signature<SpriteSystem>(signature);
     }
 
-    auto control_system = GCR.register_system<ControlSystem>();
+    auto movement_system = GCR.register_system<MovementSystem>(); // rename to movement system
     {
         Signature signature;
         signature.set(GCR.get_component_type<Transform>());
         signature.set(GCR.get_component_type<Player>());
-        GCR.set_system_signature<ControlSystem>(signature);
+        signature.set(GCR.get_component_type<Controllable>());
+        signature.set(GCR.get_component_type<Hitbox>());
+        GCR.set_system_signature<MovementSystem>(signature);
     }
 
-    control_system->init();
+    movement_system->init();
 
-    std::vector<Entity> entities(MAX_ENTITIES - 1);
+    // ProjectileSystem will need to listen to key events like control system
+    auto projectile_system = GCR.register_system<ProjectileSystem>();
+    {
+        Signature signature;
+        signature.set(GCR.get_component_type<Player>());
+        // @TODO: not sure if we edit Sprite or Projectile
+        signature.set(GCR.get_component_type<Sprite>());
+        signature.set(GCR.get_component_type<Projectile>());
+        GCR.set_system_signature<MovementSystem>(signature);
+    }
+
+    projectile_system->init();
+
+    // CollisionSystem will need to figure out what is colliding?
+    // - need to check enemy colliding with player
+    // - need to check player collidng with enemy
+//    auto collision_system = GCR.register_system<CollisionSystem>() {
+//        Signature signature;
+//        signature.set(GCR.get_component_type<Hitbox>());
+//    }
+
 
     // create the player
-    entities[0] = GCR.create_entity();
-    GCR.add_component(entities[0], Sprite{
+    Entity player = GCR.create_entity();
+    GCR.add_component(player, Sprite{
         .shader = &def_shader,
         .texture = &def_texture,
+        .scale_factor = glm::vec3(0.25f),
+        .active = true,
         .vertex_data = v,
         .vertex_count = 15 // @TODO: HARDCODED
     });
-
-    GCR.add_component(entities[0], Transform{});
-    GCR.add_component(entities[0], Player{});
-
-    auto& sprite = GCR.get_component<Sprite>(entities[0]);
+    GCR.add_component(player, Transform{});
+    GCR.add_component(player, Player{});
+    GCR.add_component(player, Controllable{});
+    GCR.add_component(player, Hitbox{});
+    auto& sprite = GCR.get_component<Sprite>(player);
     sprite.setup();
+
+    // player projectiles
+
+    // laser...
+    Entity player_laser = GCR.create_entity();
+    GCR.add_component(player_laser, Sprite {
+        .shader = &def_shader,
+        .texture = &laser_texture,
+        .active = false,
+        .vertex_data = lv,
+        .vertex_count = 20 // @TODO: HARDCODED
+    });
+
+    GCR.add_component(player_laser, Transform{});
+    GCR.add_component(player_laser, Hitbox{});
+    GCR.add_component(player_laser, Velocity{});
+    GCR.add_component(player_laser, Player{});
+    GCR.add_component(player_laser, Projectile{});
+    auto& laser_sprite = GCR.get_component<Sprite>(player_laser);
+    laser_sprite.setup();
+
+    // coming soonish
 
     float dt = 0.0f;
 
     while (!quit) {
         auto start = (float)glfwGetTime();
         window_manager.process_events();
-        control_system->update(dt);
+        movement_system->update(dt);
+        projectile_system->update(dt);
         // move this
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
