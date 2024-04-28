4d346395330c121e970ca3c1196982a9e9f37579
Will Alicar <will@alicar.me>
2024-04-27 17:30:46 -0700

    remove fnv1a_32 hashing

--- src/ecs/types.h
+++ src/ecs/types.h
@@ -28,31 +28,19 @@ enum class InputButtons
 using EventId = std::uint32_t;
 using ParamId = std::uint32_t;
 
-
-// C++11 compile-time hash of literal strings
-constexpr std::uint32_t fnv1a_32(char const* s, std::size_t count)
-{
-    return ((count ? fnv1a_32(s, count - 1) : 2166136261u) ^ s[count]) * 16777619u; // NOLINT (hicpp-signed-bitwise)
-}
-
-constexpr std::uint32_t operator "" _hash(char const* s, std::size_t count)
-{
-    return fnv1a_32(s, count);
-}
-
 namespace Events::Window {
-    const EventId QUIT = "Events::Window::QUIT"_hash;
-    const EventId RESIZED = "Events::Window::RESIZED"_hash;
-    const EventId INPUT = "Events::Window::INPUT"_hash;
+    const EventId QUIT = 8080;
+    const EventId RESIZED = 8081;
+    const EventId INPUT = 8082;
 }
 
 namespace Events::Window::Input {
-    const ParamId INPUT = "Events::Window::Input::INPUT"_hash;
+    const ParamId INPUT = 8083;
 }
 
 namespace Events::Window::Resized {
-    const ParamId WIDTH = "Events::Window::Resized::WIDTH"_hash;
-    const ParamId HEIGHT = "Events::Window::Resized::HEIGHT"_hash;
+    const ParamId WIDTH = 8084;
+    const ParamId HEIGHT = 8085;
 }
 
 // function binding, find a way to simplify this?
