0cb1d3c9d8b24fbef4ef31906b31a888949a42c3
Will Alicar <will@alicar.me>
2024-04-27 17:21:14 -0700

    make player sprite smaller

--- src/systems/sprite_system.cpp
+++ src/systems/sprite_system.cpp
@@ -6,11 +6,11 @@
 #include <gtc/type_ptr.hpp>
 
 extern Coordinator GCR;
-
 void SpriteSystem::update(float time)
 {
     glm::mat4 rotation = glm::mat4(1.0f);
     rotation = glm::rotate(rotation, time, glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::vec3 scale_factor = glm::vec3(0.25f, 0.25f, 0.25f);
 
     // fixed update
     for (auto const& entity : entities)
@@ -18,16 +18,17 @@ void SpriteSystem::update(float time)
         auto& sprite = GCR.get_component<Sprite>(entity);
         auto& movement = GCR.get_component<Transform>(entity);
 
-        glm::mat4 translation = glm::mat4(1.0f);
-        translation = glm::translate(translation, movement.pos);
-        translation = translation * rotation;
+        glm::mat4 transform = glm::mat4(1.0f);
+        transform = glm::scale(transform, scale_factor);
+        transform = glm::translate(transform, movement.pos);
+        transform = transform * rotation;
 
         // render
         safety::entry_guard("Rendering a Sprite");
         sprite.texture->bind();
         sprite.shader->use();
         unsigned int loc = glGetUniformLocation(sprite.shader->ID, "transform");
-        glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(translation));
+        glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(transform));
         glBindVertexArray(sprite.VAO);
         glDrawArrays(GL_TRIANGLE_FAN, 0, 3); // @FIXME: COUNT IS HARDCODED MY DUDE!
     }
