f086a54ecf6e17532d6feaa136f8820a98aa729a
Will Alicar <will@alicar.me>
2024-04-28 10:36:54 -0700

    remove console writes

--- src/systems/movement_system.cpp
+++ src/systems/movement_system.cpp
@@ -16,44 +16,24 @@ void MovementSystem::update(float dt)
 
             if (buttons.test(static_cast<std::size_t>(InputButtons::SHIFT))) {
                 factor = 3.0f;
-                std::cout << " L_SHIFT ";
             }
 
             auto &transform = GCR.get_component<Transform>(entity);
-            std::cout << dt << " - CtrlSys: ";
             if (buttons.test(static_cast<std::size_t>(InputButtons::W))) {
                 transform.pos.y += (dt * factor);
-                std::cout << " W ";
             }
 
             if (buttons.test(static_cast<std::size_t>(InputButtons::S))) {
                 transform.pos.y -= (dt * factor);
-                std::cout << " S ";
             }
 
             if (buttons.test(static_cast<std::size_t>(InputButtons::A))) {
                 transform.pos.x -= (dt * factor);
-                std::cout << " A ";
             }
 
             if (buttons.test(static_cast<std::size_t>(InputButtons::D))) {
                 transform.pos.x += (dt * factor);
-                std::cout << " D ";
             }
-
-//            // shooting
-//            if (buttons.test(static_cast<std::size_t>(InputButtons::J))) {
-//                std::cout << " J ";
-//            }
-//
-//            if (buttons.test(static_cast<std::size_t>(InputButtons::K))) { // laser
-//                std::cout << " K ";
-//            }
-//
-//            if (buttons.test(static_cast<std::size_t>(InputButtons::L))) {
-//                std::cout << " L ";
-//            }
-            std::cout << "\n";
         }
 }
 
