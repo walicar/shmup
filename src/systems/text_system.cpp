4453618fc685b8f88777cc5770aff3fd9f69ebc3
Will Alicar <will@alicar.me>
2024-05-12 23:30:12 -0700

    cite learnopengl

--- src/systems/text_system.cpp
+++ src/systems/text_system.cpp
@@ -1,11 +1,8 @@
 #include "text_system.h"
 #include "../ecs/coordinator.h"
-#include <iostream>
-#include <glfw3.h>
-#include <glad.h>
-#include <ft2build.h>
-#include FT_FREETYPE_H
 #include "src/components/hitbox.h"
+
+// Credit: https://learnopengl.com/In-Practice/Text-Rendering
 
 extern Coordinator GCR;
 
@@ -19,20 +16,15 @@ void TextSystem::init(Shader &shader, FT_Library &ft, FT_Face &face) {
     GCR.add_listener(METHOD_LISTENER(Events::Game::BOMB_USED, TextSystem::bomb_used)); // lambda preferred?
 
     text_shader = &shader;
-    // set size to load glyphs as
     FT_Set_Pixel_Sizes(face, 0, 48);
 
-    // disable byte-alignment restriction
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-    // load first 128 characters of ASCII set
     for (unsigned char c = 0; c < 128; c++) {
-        // Load character glyph
         if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
             std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
             continue;
         }
-        // generate texture
         unsigned int texture;
         glGenTextures(1, &texture);
         glBindTexture(GL_TEXTURE_2D, texture);
@@ -47,12 +39,10 @@ void TextSystem::init(Shader &shader, FT_Library &ft, FT_Face &face) {
                 GL_UNSIGNED_BYTE,
                 face->glyph->bitmap.buffer
         );
-        // set texture options
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        // now store character for later use
         Character character = {
                 texture,
                 glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
@@ -78,13 +68,11 @@ void TextSystem::init(Shader &shader, FT_Library &ft, FT_Face &face) {
 }
 
 void TextSystem::render_text(std::string text, float x, float y, float scale, glm::vec3 color) {
-    // activate corresponding render state
     text_shader->use();
     glUniform3f(glGetUniformLocation(text_shader->ID, "textColor"), color.x, color.y, color.z);
     glActiveTexture(GL_TEXTURE0);
     glBindVertexArray(VAO);
 
-    // iterate through all characters
     std::string::const_iterator c;
     for (c = text.begin(); c != text.end(); c++) {
         Character ch = Characters[*c];
@@ -94,7 +82,6 @@ void TextSystem::render_text(std::string text, float x, float y, float scale, gl
 
         float w = ch.size.x * scale;
         float h = ch.size.y * scale;
-        // update VBO for each character
         float vertices[6][4] = {
                 {xpos,     ypos + h, 0.0f, 0.0f},
                 {xpos,     ypos,     0.0f, 1.0f},
@@ -104,18 +91,12 @@ void TextSystem::render_text(std::string text, float x, float y, float scale, gl
                 {xpos + w, ypos,     1.0f, 1.0f},
                 {xpos + w, ypos + h, 1.0f, 0.0f}
         };
-        // render glyph texture over quad
         glBindTexture(GL_TEXTURE_2D, ch.tex_id);
-        // update content of VBO memory
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices),
-                        vertices); // be sure to use glBufferSubData and not glBufferData
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
-        // render quad
         glDrawArrays(GL_TRIANGLES, 0, 6);
-        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-        x += (ch.advance >> 6) *
-             scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+        x += (ch.advance >> 6) * scale;
     }
     glBindVertexArray(0);
     glBindTexture(GL_TEXTURE_2D, 0);
